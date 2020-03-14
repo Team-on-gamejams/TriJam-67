54447a7b9b91b2a74d8384d65f2a46b8dddd8346
Team-on <timonsol13@gmail.com>
2020-03-14 19:37:13 +0200

    Add tiles

---
+++ DualityJam/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+	Rigidbody2D rb;
+
+	private void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+}
